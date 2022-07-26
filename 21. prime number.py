9fa1e3af32de7474d04e8469f144ec624fc9b169
Romil-Pathak <109813706+Romil-Pathak@users.noreply.github.com>
2022-07-26 23:41:34 +0530

    Update 21. prime number.py

--- 21. prime number.py
+++ 21. prime number.py
@@ -1,12 +1,9 @@
 n = int(input("Enter any positive number other than 1:"))
 if n==2:
     print("The number is a prime number")
-elif n%2 == 0:
+for i in range(2,n):
+    if n%i == 0:
         print("The number is not a prime number")
-else:
-    for i in range(2,n):
-        if n%i == 0:
-            print("The number is not a prime number")
-            break
+        break
 else:
     print("The number is a prime number")

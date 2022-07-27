07ea445688a26384dcdd1746aad592de10f76211
Romil-Pathak <109813706+Romil-Pathak@users.noreply.github.com>
2022-07-27 12:26:47 +0530

    Add files via upload

--- 9. sum of first n positive integers.py
+++ 9. sum of first n positive integers.py
@@ -2,6 +2,6 @@ n = int(input("Enter any positive integer:"))
 if n==0:
     print("The sum of given no. is 0")
 elif n>0:
-    for i in range(1,n):
+    for i in range(n-1,0,-1):
             n = n + i
     print("The sum of given integers is", n)

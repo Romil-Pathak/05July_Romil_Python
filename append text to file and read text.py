b7e2e1de438cc290b68deaed3f88377afc27c9b4
Romil-Pathak <109813706+Romil-Pathak@users.noreply.github.com>
2022-10-10 12:15:15 +0530

    Delete append text to file and read text.py

--- append text to file and read text.py
+++
@@ -1,9 +0,0 @@
-'''open("file1.txt","w")
-b = open("file1.txt", "w")
-b.write("pradip pathak")'''
-
-'''b = open("file1.txt", "a")
-b.write("rajkot")'''
-
-b = open("file1.txt","r")
-print(b.readlines(2))

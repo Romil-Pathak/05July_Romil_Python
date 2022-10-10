6c87fb6d98a14fde7cfbb5abe07b33996a31428f
Romil-Pathak <109813706+Romil-Pathak@users.noreply.github.com>
2022-10-10 12:13:40 +0530

    Delete 20. string in the middle of a string.py

--- 20. string in the middle of a string.py
+++
@@ -1,5 +0,0 @@
-str = input("Enter a string with even no. of characters only:")
-str2 = input("Enter character or string to be inserted in the middle of the first string:")
-n = int(len(str)/2)
-str3 = str[0:n] + str2 + str[n:]
-print(str3)

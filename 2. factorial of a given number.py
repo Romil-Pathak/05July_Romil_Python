d6bc40f1a3c3a6ae6fbcdfe75907bf9b0cfbec25
Romil-Pathak <109813706+Romil-Pathak@users.noreply.github.com>
2022-10-10 12:13:29 +0530

    Delete 2. factorial of a given number.py

--- 2. factorial of a given number.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter any positive number:"))
-x = 1
-if n==0:
-    print("The factorial of zero is 1")
-else:
-    for i in range(n,0,-1):
-            x = x*i
-    print("The factorial of a given number is", x)
